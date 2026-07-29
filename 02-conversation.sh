87f82ce50f779f5ad5d12f8f687300e867e90f73
BharathKumarReddy <nbkumar2103@gmail.com>
2026-07-29 05:56:56 +0530

    updated file

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo "Bharath:: Hey Kumar, How are you?"
-echo "Kumar:: Hello Bharath, I am fine. How are you doing?"
+echo "Bharath:: Hey Reddy, How are you?"
+echo "Reddy:: Hello Bharath, I am fine. How are you doing?"
 echo "Bharath:: I am fine too. What's up?"
-echo "Kumar:: Nothing, just going to Mars now. Will you come?"
+echo "Reddy:: Nothing, just going to Mars now. Will you come?"
 echo "Bharath:: Sorry, you carry on! I will come once you come back"
