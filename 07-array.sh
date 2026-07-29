6e5f86cfdf51b874def28d607430724e5915a6c2
BharathKumarReddy <nbkumar2103@gmail.com>
2026-07-29 06:21:14 +0530

    updated file

--- 07-array.sh
+++ 07-array.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-SPORTS=("CRICKET" "FOOTBALL" "HOCKEY" "TENNIS" "BADMINTON")
+SPORTS=("CRICKET" "FOOTBALL" "HOCKEY" "TENNIS" "BASKETBALL")
 
 echo "First SPORT: ${SPORTS[0]}"
-echo "Last SPORT: ${SPORTS[4]}"
-echo "Fifth SPORT: ${SPORTS[5]}"
+# echo "Last SPORT: ${SPORTS[4]}"
+# echo "Fifth SPORT: ${SPORTS[5]}"
 
-echo "All SPORTS: ${SPORTS[@]}"
+# echo "All SPORTS: ${SPORTS[@]}"
